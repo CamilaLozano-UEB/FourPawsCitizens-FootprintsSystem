636109a4ec97a6a2021c270b534a729608591bb0
Nicolas-Pena-Mogollon <nicop4643@gmail.com>
2021-06-14 02:11:38 -0500

    Modifying uri

--- src/main/java/co/edu/unbosque/FourPawsCitizens_FootprintsSystem/resources/OfficialResource.java
+++ src/main/java/co/edu/unbosque/FourPawsCitizens_FootprintsSystem/resources/OfficialResource.java
@@ -7,7 +7,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/official")
+@Path("/official/{username}")
 public class OfficialResource {
 
     /**
